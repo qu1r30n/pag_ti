0bbb5a6ffad64fe2b4261e17d6a14530509a7715
qu1r30n <qu1r30n@hotmail.com>
2024-05-05 16:14:04 -0600

    le puse tamaño a la imagen

--- ti/script.js
+++ ti/script.js
@@ -2,7 +2,7 @@ const listaDeProductos = document.getElementById('listaDeProductos');
 
 let productos = [];
 const productos_a_mover = [
-  { id: 1, nombre: 'Producto 1', imagen: 'https://via.placeholder.com/150', cantidad: 0, precio: 10, extra: "", total: 0 },
+  { id: 1, nombre: 'Producto 1', imagen: 'https://i.ibb.co/pRRP12T/asfdjk.jpg', cantidad: 0, precio: 10, extra: "", total: 0 },
   { id: 2, nombre: 'Producto 2', imagen: 'https://via.placeholder.com/150', cantidad: 0, precio: 15, extra: "", total: 0 },
   // Agrega más productos con sus precios y extras aquí
 ];
@@ -88,6 +88,8 @@ function renderizarProducto(id, nombre, imagen, cantidad, precio, extra, total, 
 
   const img = document.createElement('img');
   img.src = imagen;
+  img.style.width = "150px";
+  img.style.height = "150px";
   productoDiv.appendChild(img);
 
   const nombreElemento = document.createElement('p');
